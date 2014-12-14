4c114e00d195d086132cb5cb37841d8dc25b0567
x14552067 <Paul.Reid2@student.ncirl.ie>
2014-12-14 22:57:26 +0000

    Final Website
    WE BUILD DA WEBSITE
    
    (One and only upload of our end site due to technical problems)

---
+++ js/hovereffect.js
@@ -0,0 +1,115 @@
+$(function() {
+      $('#hoverBox1').hide();
+      $('#pic1').hover( function() { $('#hoverBox1').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#hoverBox2').hide();
+      $('#topL1').hover( function() { $('#hoverBox2').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#hoverBox3').hide();
+      $('#topR1').hover( function() { $('#hoverBox3').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#hoverBox4').hide();
+      $('#botL1').hover( function() { $('#hoverBox4').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#hoverBox5').hide();
+      $('#botR1').hover( function() { $('#hoverBox5').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#hoverBox6').hide();
+      $('#topL2').hover( function() { $('#hoverBox6').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#hoverBox7').hide();
+      $('#topR2').hover( function() { $('#hoverBox7').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#hoverBox8').hide();
+      $('#botL2').hover( function() { $('#hoverBox8').fadeToggle(300); } );
+
+});
+
+
+$(function() {
+      $('#hoverBox9').hide();
+      $('#botR2').hover( function() { $('#hoverBox9').fadeToggle(300); } );
+
+});
+
+
+$(function() {
+      $('#hoverBox10').hide();
+      $('#pic2').hover( function() { $('#hoverBox10').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#a1').hide();
+      $('#artistOne').hover( function() { $('#a1').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#a2').hide();
+      $('#artistTwo').hover( function() { $('#a2').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#a3').hide();
+      $('#artistThree').hover( function() { $('#a3').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#a4').hide();
+      $('#artistFour').hover( function() { $('#a4').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#a5').hide();
+      $('#artistFive').hover( function() { $('#a5').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#a6').hide();
+      $('#artistSix').hover( function() { $('#a6').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#a7').hide();
+      $('#artistSeven').hover( function() { $('#a7').fadeToggle(300); } );
+
+});
+
+$(function() {
+      $('#a8').hide();
+      $('#artistEight').hover( function() { $('#a8').fadeToggle(300); } );
+
+});
+
+
+
+
+
+
---
+++ js/kappa.js
@@ -0,0 +1,9 @@
+$(function()
+{
+     $("#dropButton").click(function()
+     {
+         $("#slideoutContainer, #leftSlideout, #midSlideout, #rightSlideout").slideToggle();
+         return false;
+     }); 
+});
+
